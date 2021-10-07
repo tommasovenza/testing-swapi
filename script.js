a9e558b02de36c498a96816110450137d188c6b3
Tommaso <tommasovenza@gmail.com>
2021-10-07 16:08:42 +0200

    add changeOrder function

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 // this goes before endpoint
 const baseURL = "https://swapi.dev/api/planets/"
 
+// get things I need
 const planetList = document.querySelector(".planet-list")
 const planetAttr = planetList.querySelector("[data-planet-created]")
+const button = document.querySelector("#makeCall")
+const changeOrder = document.querySelector("#change-order")
 
-getData(baseURL)
+button.addEventListener("click", () => {
+  getData(baseURL)
+})
+
+changeOrder.addEventListener("click", () => {
+  dataInverse(baseURL)
+})
+
+function dataInverse(URL) {
+  planetList.innerHTML = ""
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const planets = data.results
+
+      planets
+        .slice()
+        .reverse()
+        .forEach((planet) => {
+          const allPlanet = []
+          const planetName = planet.name
+          const planetCreated = planet.created
+
+          const hourCreated = planetCreated.slice(11, 19)
+          const dayCreated = planetCreated.slice(0, 10)
+          console.log()
+          const planetDom = document.createElement("li")
+          // push every planet in array
+          allPlanet.push(planetCreated)
+          // create a span
+          const planetDate = document.createElement("span")
+          planetDom.classList.add("planet")
+          planetDom.innerHTML = planetName
+          planetDate.innerHTML = dayCreated + " " + hourCreated
+          planetList.appendChild(planetDom)
+          planetDom.appendChild(planetDate)
+        })
+    })
+}
 
 function getData(URL) {
+  planetList.innerHTML = ""
   fetch(URL)
     .then((response) => response.json())
     .then((data) => {
@@ -45,11 +88,11 @@ function getData(URL) {
     })
 }
 
-function reverseString(string) {
-  var result = ""
-  for (var i = string.length - 1; i >= 0; i--) result += string[i]
-  return result
-}
+// function reverseString(string) {
+//   var result = ""
+//   for (var i = string.length - 1; i >= 0; i--) result += string[i]
+//   return result
+// }
 
 // search parameters
 // https://swapi.dev/api/people/?search=r2
