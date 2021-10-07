300682e1c4b80a56752decf665317b4e7776a54a
Tommaso <tommasovenza@gmail.com>
2021-10-07 14:53:22 +0200

    add google font & some styles

--- script.js
+++ script.js
@@ -3,6 +3,54 @@ const baseURL = "https://swapi.dev/api/planets/"
 
 const planetList = document.querySelector(".planet-list")
 const planetAttr = planetList.querySelector("[data-planet-created]")
+
+getData(baseURL)
+
+function getData(URL) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const planets = data.results
+
+      planets.forEach((planet) => {
+        const allPlanet = []
+        const planetName = planet.name
+        const planetCreated = planet.created
+
+        const hourCreated = planetCreated.slice(11, 19)
+        const dayCreated = planetCreated.slice(0, 10)
+        console.log()
+        const planetDom = document.createElement("li")
+        // push every planet in array
+        allPlanet.push(planetCreated)
+        // create a span
+        const planetDate = document.createElement("span")
+        planetDom.classList.add("planet")
+        planetDom.innerHTML = planetName
+        planetDate.innerHTML = dayCreated + " " + hourCreated
+        planetList.appendChild(planetDom)
+        planetDom.appendChild(planetDate)
+
+        // const allPlanetDom = Array.from(document.querySelectorAll(".planetDom"))
+        // console.log(allPlanetDom)
+        // for (let index = 0; index < allPlanet.length; index++) {
+        //   const currentEl = allPlanet[index]
+        //   console.log(currentEl)
+        //   //   const planetDate = document.createElement("span")
+
+        //   //   planetDate.innerHTML = currentEl.textContent
+        //   //   planetDom.appendChild(planetDate)
+        // }
+      })
+    })
+}
+
+function reverseString(string) {
+  var result = ""
+  for (var i = string.length - 1; i >= 0; i--) result += string[i]
+  return result
+}
+
 // search parameters
 // https://swapi.dev/api/people/?search=r2
 
@@ -16,23 +64,3 @@ const planetAttr = planetList.querySelector("[data-planet-created]")
 // }
 
 // getData(baseURL)
-
-getData(baseURL)
-
-function getData(URL) {
-  fetch(URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const planets = data.results
-
-      planets.forEach((planet) => {
-        const planetName = planet.name
-        const planetCreated = planet.created
-        const planetDom = document.createElement("div")
-        planetDom.classList.add("planet")
-        planetDom.innerHTML = planetName
-        planetDom.dataset.planetCreated = planetCreated
-        planetList.appendChild(planetDom)
-      })
-    })
-}
